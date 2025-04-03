4e521279c0bfd8107cab8e852946ccdfae6731c0
Ryan Clark <112436993+Varian-RC@users.noreply.github.com>
2025-04-03 11:23:49 -0700

    Update Expiration

--- ModulationComplexity/Properties/AssemblyInfo.cs
+++ ModulationComplexity/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyExpirationDate("4/1/2025")]
+[assembly: AssemblyExpirationDate("8/1/2025")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyInformationalVersion("1.0")]
